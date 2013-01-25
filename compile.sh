371e950a6dcfa815cb2418f76fc19b77ad8fe236
skinner12 <mazzu83@gmail.com>
2013-01-25 18:38:26 +0000

    first build

---
+++ compile.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+BASE=/home/tux/apps/nexus/
+KERNEL=kernel/marmite
+ZIP=zipKernel
+DATE=$(date +"%Y%m%d%H%M")
+
+
+
+cd $BASE$KERNEL
+git branch
+read -p "Correct branch? [Y/N]: " -n 1
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+	then
+	    	echo -e "\n"
+	        exit 1
+        fi
+
+echo -e "\nSTARTING...\n"
+
+export PATH=$PATH:/home/tux/apps/nexus/toolchain/ZenKernel_ARCH_armv7a_compiler/bin/
+#export PATH=$PATH:/home/tux/apps/nexus/toolchain/arm-2012.09/bin/
+export ARCH=arm
+export SUBARCH=arm
+export CROSS_COMPILE=arm-zen-linux-gnueabi-
+#export CROSS COMPILE=arm-none-eabi-
+
+#cp arch/arm/configs/crespo_dave_config ./.config
+make -j4
+
+echo -e "Checking result...\n"
+ls -l $BASE$KERNEL/arch/arm/boot/zImage
+
+echo -e "Copying Files...\n"
+cp $BASE$KERNEL/arch/arm/boot/zImage $BASE$KERNEL/mkboot/
+cd $BASE$KERNEL/mkboot
+./mkbootimg --kernel zImage --ramdisk cyan2disk_new.cpio.gz --cmdline 'no_console_suspend=1 console=bull's --base 0x30000000 --pagesize 4096 -o boot.img
+
+echo -e "Creating ZIP...\n"
+cp $BASE$KERNEL/mkboot/boot.img $BASE$ZIP/boot.img
+cp $BASE$KERNEL/drivers/scsi/scsi_wait_scan.ko $BASE$ZIP/system/modules/
+7z a -r -tzip $BASE/mykernel-$DATE.zip $BASE$ZIP/*
+
+make clean
+rm -rfv $BASE$KERNEL/mkboot/boot.img
+rm -rfv $BASE$ZIP/boot.img
+rm -rfv $BASE$ZIP/system/modules/scsi_wait_scan.ko
---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+BASE=/home/tux/apps/nexus/
+KERNEL=kernel/marmite
+DATE=$(date +"%Y%m%d%H%M")
+
+
+
+echo -e "Directory kernel $BASE/$KERNEL \n"
+
+ls -l $BASE$KERNEL
+
+echo -e "\n data oggi $DATE \n"
